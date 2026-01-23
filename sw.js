103f546b7f5a10636e55bce20a8d954731bf3647
Pedro Harenza <pedro.harenza@ageon.com.br>
2026-01-23 09:21:32 -0300

    Remoção do overscroll effects

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Nome e versão do cache. MUDE A VERSÃO a cada nova atualização.
-const cacheName = 'dashboard-v1.4.2'; 
+const cacheName = 'dashboard-v1.4.3'; 
 
 // Arquivos que o app vai salvar para abrir rápido
 const staticAssets = [
